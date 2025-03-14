7596f63676b10b731ca351e51d1f61eed426369d
Ilyass Bougati <110248648+Ilyass-Bougati@users.noreply.github.com>
2025-03-14 03:17:47 +0000

    Added string verification functions

--- db/functions.sql
+++ db/functions.sql
@@ -1,15 +1,7 @@
-CREATE FUNCTION dbo.TRIMME (@input_text NVARCHAR(MAX))  
+CREATE FUNCTION dbo.Trim (@input NVARCHAR(MAX))  
 RETURNS NVARCHAR(MAX)  
 AS  
 BEGIN  
-    -- Supprimer les espaces en début et fin  
-    DECLARE @trimmed_text NVARCHAR(MAX) = LTRIM(RTRIM(@input_text));  
-
-    -- Remplacer plusieurs espaces internes par un seul  
-    WHILE CHARINDEX('  ', @trimmed_text) > 0  
-    BEGIN  
-        SET @trimmed_text = REPLACE(@trimmed_text, '  ', ' ');  
-    END  
-
-    RETURN @trimmed_text;  
-END
+    RETURN LTRIM(RTRIM(@input))  
+END  
+GO  
