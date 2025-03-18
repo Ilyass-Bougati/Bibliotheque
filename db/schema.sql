d7ad850cdf517b222b4f135fd9a7473d6162b74c
abdellahmp <abdellah.darni.2018@gmail.com>
2025-03-18 06:14:32 +0000

    Change column name TREVIEWS   Review ----> Commentaire

--- db/schema.sql
+++ db/schema.sql
@@ -118,7 +118,7 @@ CREATE TABLE TREVIEWS (
   IdClient INT NOT NULL ,
   IdLivre INT NOT NULL,
   Notation INT CHECK (Notation BETWEEN 1 AND 10),
-  Review NVARCHAR(MAX) NOT NULL,
+  Commentaire NVARCHAR(MAX) NULL,
   CONSTRAINT FK_REVIEWS_CLIENT FOREIGN KEY (IdClient)
       REFERENCES TCLIENTS(IdClient), -- it's ok to have a reviews even if the client is gone
   CONSTRAINT FK_REVIEWS_LIVRE FOREIGN KEY (IdLivre)
