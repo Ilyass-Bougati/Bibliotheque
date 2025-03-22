29fa46feb1ecc635812809a7d74471973cfcf827
Ilyass Bougati <110248648+Ilyass-Bougati@users.noreply.github.com>
2025-03-22 20:28:50 +0000

    Checking if a string is empty

---
+++ C/include/utils.c
@@ -0,0 +1,8 @@
+#include "utils.h"
+#include "stdbool.h"
+
+bool check_empty(char *str)
+{
+    // TODO : trim the string before checking
+    return str[0] == '\0';
+}
--- C/include/utils.h
+++ C/include/utils.h
@@ -5,3 +5,9 @@
  * @param str la chaîne de caractères à couper
  */
 char *trim(char *str);
+
+/**
+ * cette fonction vérifie si une chaîne de caractères est vide ou remplie d'espaces
+ * @param str la chaîne de caractères à vérifier
+ */
+bool check_empty(char *str);
