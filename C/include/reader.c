7864c3766718e1944bb78e534f3f6c8571ee942e
AmineSidki <amineyoutubw@gmail.com>
2025-03-23 20:13:33 +0000

    made the file_reader function

--- C/include/reader.c
+++ C/include/reader.c
@@ -13,3 +13,32 @@ char *saisir_chaine()
     str[i] = '\0';
     return str;
 }
+
+char *read_file(char *file_name)
+{
+    if(check_empty(trim(file_name)))
+    {
+        fprintf(stderr , "Nom du fichier invalide .\n");
+        return NULL;
+    }
+
+    FILE* file_pointer = fopen(file_name , "r");
+    if(file_pointer == NULL)
+    {
+        fprintf(stderr , "Fichier introuvable .\n");
+        return NULL;
+    }
+
+    char c ;
+    int i = 0;
+    char* file_content = malloc(sizeof(char));
+
+    while((c = fgetc(file_pointer)) != EOF)
+    {
+        file_content[i++] = c;
+        file_content = realloc(file_content , (i+1)*sizeof(char));
+    }
+    file_content[i] = '\0';
+
+    return file_content;
+}
