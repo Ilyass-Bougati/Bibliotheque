86d931e71fdf2b2c7ce1f72c2993c51409d07fbf
Ilyass Bougati <110248648+Ilyass-Bougati@users.noreply.github.com>
2025-03-26 12:33:13 +0000

    Les fonctions de livre.h

--- C/include/livre.h
+++ C/include/livre.h
@@ -4,3 +4,20 @@ typedef struct Livre
 {
     /* data */
 } Livre;
+
+// cette variable contient le dernier identifiant livre généré
+int last_livre_id = 0;
+
+/**
+ * cette fonction transformera une chaîne en une structure livre
+ * @param str la chaîne à convertir
+ * @return renvoie NULL si le format n'est pas valide
+ */
+Livre *string_to_livre(char *str);
+
+/**
+ * Cette fonction transformera une structure livre en une chaîne de caractères à écrire dans un fichier
+ * @param livre la structure livre à convertir
+ * @return renvoie NULL en cas d'erreur
+ */
+char *livre_to_string(Livre livre);
