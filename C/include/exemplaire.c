99d1013a64556201e3f93c93013e8211f7638e30
Ilyass Bougati <110248648+Ilyass-Bougati@users.noreply.github.com>
2025-03-27 21:50:57 +0000

    Exemplaires table

---
+++ C/include/exemplaire.c
@@ -0,0 +1,91 @@
+#include "exemplaire.h"
+#include "utils.h"
+#include "stdlib.h"
+#include "stdio.h"
+#include "reader.h"
+#include "string.h"
+
+
+Exemplaire *string_to_exemplaire(char *str)
+{
+    int length;
+    char **split_string = split(str, delimiter, &length);
+
+    // checking if all the fields are present
+    if (length != 3)
+    {
+        printf("Erreur de lecture du fichier exemplaire");
+        return NULL;
+    }
+
+    // Creating the exemplaire
+    Exemplaire *exemplaire   = (Exemplaire*) malloc(sizeof(Exemplaire));
+    exemplaire->id           = atoi(split_string[0]);
+    exemplaire->disponiblite = atoi(split_string[1]);
+    exemplaire->localisation = split_string[2];
+
+    return exemplaire;
+}
+
+char *exemplaire_to_string(Exemplaire *exemplaire)
+{
+    char id[10], disponible[10];
+    itoa(exemplaire->id, id, 10);
+    itoa(exemplaire->disponiblite, disponible, 10);
+    char *buffer = (char *) calloc(2048,  sizeof(char));
+    strcat(buffer, id);
+    strcat(buffer, "#");
+    strcat(buffer, disponible);
+    strcat(buffer, "#");
+    strcat(buffer, exemplaire->localisation);
+
+    return buffer;
+}
+
+Exemplaire **load_exemplaires(int *length)
+{
+    // the list of Exemplaires
+    Exemplaire **exemplaires = (Exemplaire**) malloc(sizeof(Exemplaire *));
+    *length = 0;
+
+    char *file = read_file(ficher_exemplaires);
+    if (file == NULL)
+    {
+        printf("Erreur de lecture du fichier exemplaire\n");
+        return NULL;
+    }
+
+    int len;
+    char **exemplaire_strings = split(file, '\n', &len);
+
+    for (int i = 0; i < len; i++)
+    {
+        Exemplaire *exemplaire = string_to_exemplaire(exemplaire_strings[i]);
+        if (exemplaire == NULL)
+        {
+            printf("Erreur format de fichier\n");
+            return NULL;
+        }
+
+        exemplaires[(*length)++] = exemplaire;
+        exemplaires = (Exemplaire**) realloc(exemplaires, (*length + 1) * sizeof(Exemplaire*));
+    }
+
+    return exemplaires;
+}
+
+void save_exemplaires(Exemplaire **exemplaires, int number)
+{
+    FILE *fptr = fopen(ficher_exemplaires, "w");
+    if (fptr == NULL)
+    {
+        printf("Erreur de lecture du fichier exemplaire\n");
+        return;
+    }
+
+    for (int i = 0; i < number; i++)
+    {
+        fprintf(fptr, "%s\n", exemplaire_to_string(exemplaires[i]));
+    }
+    fclose(fptr);
+}
--- C/include/exemplaire.h
+++ C/include/exemplaire.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#ifndef ficher_exemplaires
+    #define ficher_exemplaires "data/exemplaires"
+#endif
+
 typedef enum Disponibilite
 {
     disponible
@@ -12,3 +16,32 @@ typedef struct Exemplaire
     char *localisation;
     // add link to livre
 } Exemplaire;
+
+
+/**
+ * Cette fonction transformera une chaîne en une structure exemplaire
+ * @param str la chaîne à convertir
+ * @return renvoie NULL si le format n'est pas valide
+ */
+Exemplaire *string_to_exemplaire(char *str);
+
+/**
+ * Cette fonction transformera une structure exemplaire en une chaîne de caractères à écrire dans un fichier
+ * @param exemplaire la structure exemplaire à convertir
+ * @return renvoie NULL en cas d'erreur
+ */
+char *exemplaire_to_string(Exemplaire *exemplaire);
+
+/**
+ * Cette fonction charge tous les exemplaires sur le fichier exemplaire
+ * @param length cette variable contient la taille de la table renvoyée
+ */
+Exemplaire **load_exemplaires(int *length);
+
+/**
+ * Cette fonction enregistre tous les exemplaires dans le fichier
+ * @param exemplaires Les exemplaires à sauvegarder
+ * @param number la taille du tableau exemplaires
+ */
+void save_exemplaires(Exemplaire **exemplaires, int number);
+
