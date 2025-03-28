797abbc1fa00ef1204519525365d88e8002957df
AmineSidki <amineyoutubw@gmail.com>
2025-03-28 01:22:21 +0000

    Alrighty

--- C/biblio.c
+++ C/biblio.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "include/emprunt.h"
+#include "include/reservation.h"
 #include "include/utils.h"
 
 
 int main()
 {
-	//In use for testing..
+	
+
 	return 0;
 }
--- C/test.c
+++ C/test.c
@@ -1,9 +1,9 @@
-#include <string.h>
-#include <ctype.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include "utils.h"
-#include "stdbool.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include "include/utils.h"
 
 bool check_empty(char *str)
 {
@@ -209,27 +209,6 @@ char* new_strrev(char* str)
     return strreved;
 }
 
-char* ftoa(float x)
-{
-    int integer_part = (int)x;
-    x -= integer_part;
-    
-    while(x - (int)x != 0)
-    {
-        x *= 10;
-    }
-
-    char *int_str   = new_itoa(integer_part),
-         *float_str = new_itoa((int)x)      ,
-         *out       = malloc((strlen(int_str) + strlen(float_str) + 2)*sizeof(char));
-        
-    strcat(out , int_str);
-    strcat(out , ".");
-    strcat(out , float_str);
-
-    return out;
-}
-
 char* new_itoa(int x)
 {
     bool isNegative = x < 0 ? true : false ;
@@ -285,4 +264,196 @@ char **split(char *str, char d, int *length)
     table[(*length)++] = word;
 
     return table;
+}
+
+#define BAD_DATE (Date){0 , 0 , 0}
+
+typedef struct {
+    int id;
+    char *nom_langue;
+} Langue;
+
+typedef struct {
+    int id;
+    char *nom_auteur;
+    char *prenom_auteur;
+} Auteur;
+
+typedef struct {
+    int id;
+    char *nom_categorie;
+} Categorie;
+
+typedef struct {
+    int id;
+    char *nom_editeur;
+} Editeur;
+
+typedef struct {
+    int id;
+    char *titre;
+    char *isbn;
+    
+    Langue* langue;
+    
+    Auteur** auteurs;
+    int nombreAuteurs;
+    
+    Categorie** categories;
+    int nombreCategories;
+    
+    Editeur** editeurs;
+    int nombreEditeurs;
+
+} Livre;
+
+typedef struct Date
+{
+    int jour;
+    int mois;
+    int annee;
+} Date;
+
+typedef struct Reservation
+{
+    int id;
+    int id_client;
+    Livre *livre;
+    Date date_reservation;
+} Reservation;
+
+Date string_to_date(char *str)
+{
+    if(str == NULL)
+    {
+        goto inv_date;
+    }
+
+    int len ;
+    char **splitted_str = split(str , '/' , &len) ;
+    int i , j , m , a ;
+
+    for(i = 0 ; i < len ; i++)
+    {
+        int j;
+        for(j = 0 ; splitted_str[i][j] != '\0' ; j++)
+        {
+            if(splitted_str[i][j] < '0' || splitted_str[i][j] > '9')
+            {
+                goto inv_date;
+            }
+        }
+    }
+
+    if(i != 3)
+    {
+        goto inv_date;
+    }
+
+    j = atoi(splitted_str[0]);
+    m = atoi(splitted_str[1]);
+    a = atoi(splitted_str[2]);
+
+    if(j < 1 || m < 1 || a < 1)
+    {
+        goto inv_date;
+    }
+
+
+    if(m > 12 || m < 1)
+    {
+        goto inv_date;
+    }
+
+    int Mois[13] = {0 , 31 , 28 , 31 , 30 , 31 , 30 , 31 , 31 , 30 , 31 , 30 , 31} ;
+    if(a % 100 == 0 || a % 4 == 0)
+    {
+        Mois[2]++;
+    }
+
+    if(j > Mois[m])
+    {
+        goto inv_date;
+    }
+
+    Date date = (Date){j , m , a};
+
+    return date;
+
+    inv_date :
+        return BAD_DATE;
+}
+
+char *date_to_string(Date date)
+{
+    char *j = new_itoa(date.jour) , *m = new_itoa(date.mois) , *a = new_itoa(date.annee);
+    int l1 = strlen(j) , l2 = strlen(m) , l3 = strlen(a);
+
+    char* out = malloc((l1 + l2 + l3 + 3) * sizeof(char));
+
+    out[0] = '\0';
+
+    strcat(out , j);
+    strcat(out , "/");
+
+    strcat(out , m);
+    strcat(out , "/");
+
+    strcat(out , a);
+
+    return out;
+}
+
+
+int datecmp(Date date1 , Date date2)
+{
+    int annee_diff , mois_diff , jour_diff;
+    if(annee_diff = (date1.annee - date2.annee))
+    {
+        return annee_diff > 0 ? 1 : -1 ;
+    }
+    else
+    {
+        if(mois_diff = (date1.mois - date2.mois))
+        {
+            return mois_diff > 0 ? 1 : -1 ;
+        }
+        else
+        {
+            if(jour_diff = (date1.jour - date2.jour))
+            {
+                return jour_diff > 0 ? 1 : -1 ;
+            }
+            return 0;
+        }
+    }
+}
+
+
+char* ftoa(float x)
+{
+    int integer_part = (int)x;
+    x -= integer_part;
+    
+    while(x - (int)x != 0)
+    {
+        x *= 10;
+    }
+
+    char *int_str   = new_itoa(integer_part),
+         *float_str = new_itoa((int)x)      ,
+         *out       = malloc((strlen(int_str) + strlen(float_str) + 2)*sizeof(char));
+        
+    strcat(out , int_str);
+    strcat(out , ".");
+    strcat(out , float_str);
+
+    return out;
+}
+
+int main()
+{
+	float x = 22.01;
+	printf("%s" , ftoa(x));
+	return 0;
 }
